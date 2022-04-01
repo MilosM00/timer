adde3461762951d1600d5e5b27453be93674b003
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-01 19:19:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+const timer = document.querySelector(`.timer`);
+const buttonStart = document.querySelector(`.start`);
+
+const hours = document.querySelector(`.hours`);
+const minutes = document.querySelector(`.minutes`);
+const seconds = document.querySelector(`.seconds`);
+
+
