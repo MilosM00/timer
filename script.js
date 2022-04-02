37df4ecef2c4d4a451ac96dbddf5443b22df3164
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-02 23:24:45 +0200

    updated

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ const minutes = document.querySelector(`.minutes`);
 const seconds = document.querySelector(`.seconds`);
 
 
+const addHours = function(id){
+    const select = id;
+    const hour = 23;
+
+    for(let i = 0; i <= hour; i++){
+        select.options[select.options.length] = new Option(i < 10 ? `0` + i : i);
+    }
+};
+addHours(hours);
+
+const addMinutes = function(id){
+    const select = id;
+    const minute = 59;
+
+    for(let i = 0; i <= minute; i++){
+        select.options[select.options.length] = new Option(i < 10 ? `0` + i : i);
+    }
+};
+addMinutes(minutes);
+
+const addSeconds = function(id){
+    const select = id;
+    const second = 59;
+
+    for(let i = 0; i <= second; i++){
+        select.options[select.options.length] = new Option(i < 10 ? `0` + i : i);
+    }
+};
+addMinutes(seconds);
